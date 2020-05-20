476c43a97fa5ba9fc59de2f1f0a3a81c75169e14
fatimanawal3 <62928980+fatimanawal3@users.noreply.github.com>
2020-05-20 13:16:56 +0530

    Add files via upload

---
+++ view/header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Administrator</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link  rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+
+        <!-- MetisMenu CSS -->
+        <link href="assets/js/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="assets/css/sb-admin-2.css" rel="stylesheet">
+        <!-- Custom Fonts -->
+        <link href="assets/fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+        <script src="assets/js/jquery.min.js" type="text/javascript"></script>
+
+    </head>
+
+    <body>
+
+        <div id="wrapper">
+
+            <!-- Navigation -->
+            <?php if (isset($_SESSION['user_logged_in']) && $_SESSION['user_logged_in'] == true):
+                $logtyp = $_SESSION['admin_type'];
+                ?>
+                <nav class="navbar navbar-default navbar-top" role="navigation" style="margin-bottom: 0">
+                    <?php if ($logtyp == 'admin') {?>
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="index.php">Home</a>
+						
+                        
+                    </div>
+                    <?php } ?>
+
+                    <ul class="nav navbar-top-links navbar-right">
+                        <!-- /.dropdown -->
+						<a class="navbar-brand" href="logout.php"> Logout</a>
+                    
+                    </ul>
+                    <!-- /.navbar-top-links -->
+
+                    <div class="navbar-default sidebar" role="navigation">
+                        <div class="sidebar-nav navbar-collapse">
+                            <ul class="nav" id="side-menu">
+                                <li><a href="event_wall.php">Event wall</a></li>
+                                <li><a href="ai_virtual_cam.php">AI virtual camera</a></li>
+                                <li><a href="timetravel_channel.php">Time travel</a></li>
+                                <li><a href="views.php">Heat map</a></li>
+                                
+                            </ul>
+                        </div>
+                        <!-- /.sidebar-collapse -->
+                    </div>
+                    <!-- /.navbar-static-side -->
+                </nav>
+            <?php endif; ?>
+            <!-- The End of the Header -->
